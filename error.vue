069def63fe73231f2345565f6786885810edc6e7
Santiago Toyos <ppikachu@gmail.com>
2022-08-25 16:53:09 -0300

    fix error

--- error.vue
+++ error.vue
@@ -4,7 +4,7 @@
       <h1 v-if="error.statusCode === 404" class="my-4 text-6xl tracking-tighter text-primary">404</h1>
       <h1 v-else class="my-4 text-6xl tracking-tighter">💣 <span class="text-primary">PUM!</span></h1>
       <code class="my-2 text-gray-500">{{ error.statusCode }}: {{ error.message }}</code>
-      <img src="/images/et1.gif" class="my-8 rounded max-w-sm" alt="404" />
+      <img src="/images/ET_en_nave_transparente.gif" class="my-8 rounded max-w-sm" alt="404" />
       <button class="btn btn-error" @click="handleError">Mmmm. Volver al inicio</button>
     </div>
   </div>
