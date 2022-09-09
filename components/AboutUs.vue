b955ed1dc1d5f3f6978b4af392a1b17533d89c0d
Santiago Toyos <ppikachu@gmail.com>
2022-09-09 15:51:03 -0300

    Update AboutUs.vue

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -15,7 +15,7 @@ required: false,
         :class="{'text-primary': props.ciclo === 'night', 'text-neutral': props.ciclo === 'day'}"
         class="text-4xl md:text-5xl px-4 lg:text-left my-4 font-bold max-w-md leading-tight lg:leading-none"
         >
-        LUZ NEGRA es un Estudio Multimedia que produce Gráfica Animada
+        LUZ NEGRA es el Estudio Multimedia que buscabas.
         </h1>
         <div
           class="text-center w-full lg:w-96 relative px-4 lg:px-0 text-3xl leading-none"
@@ -24,40 +24,31 @@ required: false,
           <img
             src="/images/ET_en_nave_transparente.gif"
             loading="lazy"
-            class="absolute -top-1/3 left-1/4 md:left-1/4 md:-top-0 w-1/2 md:w-1/2 lg:w-full lg:-top-8 lg:-left-48"
+            class="absolute left-1/4 md:left-1/3 top-40 w-1/2 md:w-1/3 lg:w-2/3 lg:-top-8 lg:-left-1/2"
             width="540"
             height="540"
             alt="Quienes somos"
           />
-          <div class="grid justify-items-stretch md:grid-cols-5 lg:grid-cols-1 gap-2">
+          <div>Nos especializamos en COMUNICACiÓN CREATIVA, dando vida a tus ideas y produciendo la mejor imagen para tus proyectos.</div>
+          <!--<div class="grid justify-items-stretch md:grid-cols-5 lg:grid-cols-1 gap-2">
             <div class="flex items-center justify-center rounded-lg bg-primary bg-opacity-10 p-4">Cine</div>
             <div class="flex items-center justify-center rounded-lg bg-primary bg-opacity-10 p-4">TV</div>
             <div class="flex items-center justify-center rounded-lg bg-primary bg-opacity-10 p-4">Redes Sociales</div>
             <div class="flex items-center justify-center rounded-lg bg-primary bg-opacity-10 p-4">Gráfica</div>
             <div class="flex items-center justify-center rounded-lg bg-primary bg-opacity-10 p-4">Web</div>
-          </div>
+          </div>-->
         </div>
       </div>
     </div>
-    <div class="hero max-w-5xl mx-auto mt-32 md:mt-64 lg:mt-32">
+    <div class="hero max-w-5xl mx-auto mt-64 md:mt-96 lg:mt-32">
       <div class="hero-content flex-col lg:flex-row-reverse text-center lg:text-left">
-        <div>
-          <h1
-            :class="{'text-primary' : props.ciclo === 'night', 'text-neutral' : props.ciclo === 'day'}"
-            class="text-4xl md:text-5xl px-4 font-bold mb-16"
-          >
-            Comunicación creativa y síntesis
-          </h1>
           <div
             class="prose"
             :class="{'text-neutral' : props.ciclo === 'day', 'text-neutral-content' : props.ciclo === 'night'}"
           >
-            <p class="my-4 text-2xl">LUZ NEGRA está dirigido por Inés Trigub y Santiago Toyos y cuenta con un equipo de
-              profesionales con amplia experiencia.</p>
-            <p class="my-4 text-2xl">Nos especializamos en COMUNICACiÓN CREATIVA, dando vida a las ideas y ofreciendo la
-              mejor imagen para tus proyectos.</p>
+            <p class="my-4 text-2xl">LUZ NEGRA está dirigido por Inés Trigub y Santiago Toyos.  Junto a un equipo de profesionales con amplia experiencia, producimos VIDEO, ANIMACIÓN, MOTION GRAPHICS, DISEÑO GRÁFICO y DISEÑO WEB, combinando técnicas tradicionales con las nuevas tecnologías.</p>
+            <p class="my-4 text-2xl">Buscamos la síntesis necesaria para comunicar de manera original y efectiva tu mensaje.</p>
           </div>
-        </div>
         <img
           src="/images/bety.gif"
           loading="lazy"
