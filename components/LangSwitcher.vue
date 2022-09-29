0bf78c0cf48f38e30c5569aa9ee784d576804b48
Santiago Toyos <ppikachu@gmail.com>
2022-09-29 02:19:54 -0300

    switcher

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,19 @@
+<script setup>
+const nuxtApp = useNuxtApp()
+const availableLocales = computed(() => {
+  return nuxtApp.$i18n.locales.filter(i => i.code !== nuxtApp.$i18n.locale)
+})
+</script>
+
+<template>
+  <div class="fixed right-0 z-50 p-6">
+    <nuxt-link
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :to="switchLocalePath(locale.code)"
+      class="btn btn-primary btn-xs"
+    >
+      {{ locale.name }}
+    </nuxt-link>
+  </div>
+</template>
--- layouts/clean.vue
+++ layouts/clean.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <LangSwitcher />
     <NuxtPage />
     <SiteFooter />
   </div>
