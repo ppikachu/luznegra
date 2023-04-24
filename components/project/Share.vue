864fad586fe522a3d12b104d921258a093f1890e
Santiago Toyos <ppikachu@gmail.com>
2023-04-24 01:43:34 -0300

    share

--- components/project/Share.vue
+++ components/project/Share.vue
@@ -1,40 +1,51 @@
 <script setup lang="ts">
+import { url } from 'inspector'
+
 /* Define props */
 const props = defineProps({
-  project: { type: Object, required: true },
+	project: { type: Object, required: true },
 })
 const config = useRuntimeConfig()
-const { text, isSupported, copy, copied } = useClipboard()
+const { copy, copied } = useClipboard()
 const sound = useSound('/sounds/Click03.mp3')
-//const soundClose = useSound('/sounds/close.mp3', { volume: 0.5 })
+const canShareData = navigator.canShare
+const resultPara = ref(config.HOST + '/proyecto/' + props.project.slug)
 
-function closeProject() {
-  copy(config.HOST+'/proyecto/'+props.project.slug)
-  sound.play()
+async function copyLink() {
+	if (!navigator.canShare) {
+		// si no permite compartir, copia el link 'ctrl/cmd c'
+		copy(resultPara.value)
+	} else {
+		//si el navegador permite 'compartir'
+		try {
+		const shareData = {
+			text: props.project.title,
+			url: resultPara.value,
+		}
+			await navigator.share(shareData);
+			sound.play()
+		} catch (err) {
+			resultPara.value = `Error: ${err}`
+		}
+	}
 }
-
 </script>
 
 <template>
-  <div class="pt-4 md:pt-0">
-    <p class="text-sm mb-1 ml-3 text-zinc-400 md:text-right">{{ $t('link_compartir') }}:</p>
-    <div v-if="isSupported" class="form-control">
-      <div class="input-group input-group-sm">
-        <span class="input input-sm text-zinc-400 truncate">
-          {{ config.HOST+'/proyecto/'+project.slug }}
-        </span>
-        <div class="tooltip tooltip-primary" :data-tip="copied ? $t('copied') : $t('link_copy')">
-        <button
-          @click="closeProject"
-          class="btn btn-sm btn-square btn-secondary rounded-l-none"
-        >
-          <IconCSS name="mdi:clipboard-outline" />
-        </button>
-        </div>
-      </div>
-    </div>
-    <div v-else class="text-xs text-zinc-400">
-      <a :href="config.HOST+'/proyecto/'+project.slug" class="link link-primary">{{ project.slug }}</a>
-    </div>
-  </div>
+	<div class="pt-4 md:pt-0">
+		<!--si no permite compartir, copia el link 'ctrl/cmd c'-->
+		<div v-if="!canShareData">
+			<div class="tooltip tooltip-primary" :data-tip="copied ? $t('copied') : $t('link_copy')">
+				<button @click="copyLink" class="btn btn-sm btn-secondary space-x-2">
+					<IconCSS name="mdi:clipboard-outline" /><span>{{ $t('link_compartir') }}</span>
+				</button>
+			</div>
+		</div>
+		<!--si el navegador permite 'compartir'-->
+		<div v-else class="text-xs text-zinc-400">
+			<button @click="copyLink" class="btn btn-sm btn-secondary space-x-2">
+				<IconCSS name="mdi:clipboard-outline" /><span>{{ $t('link_compartir') }}</span>
+			</button>
+		</div>
+	</div>
 </template>
