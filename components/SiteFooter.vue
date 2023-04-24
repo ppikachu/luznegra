cad6590145c624a542738fee551620493485a0ba
Santiago Toyos <ppikachu@gmail.com>
2023-04-23 23:18:07 -0300

    config.public

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -6,8 +6,8 @@
   <footer class="footer items-center bg-base-300 text-neutral-content z-50 relative">
     <div class="container mx-auto p-4 md:p-6">
       <div class="flex flex-col space-y-2 items-center justify-center w-full text-gray-400">
-        <img :src="config.TEAM_LOGO_IMAGE_ON_FOOTER" :alt="config.TEAM_NAME" class="w-10" width="256" height="256" loading="lazy" />
-        <span>&copy; {{ new Date().getFullYear() }} {{ config.TEAM_NAME }}</span>
+        <img :src="config.public.TEAM_LOGO_IMAGE_ON_FOOTER" :alt="config.public.TEAM_NAME" class="w-10" width="256" height="256" loading="lazy" />
+        <span>&copy; {{ new Date().getFullYear() }} {{ config.public.TEAM_NAME }}</span>
       </div>
       <!--<div class="grid grid-flow-col gap-4 place-self-center justify-self-end">
         <a href="https://vimeo.com/estudioluznegra" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
